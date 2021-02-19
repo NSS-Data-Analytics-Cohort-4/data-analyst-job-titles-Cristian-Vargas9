eceac8b3783dcbf9277d0b16be947f4979a89d40
Cristian.Vargas9 <cristianvargas562@gmail.com>
2021-02-18 21:29:11 -0600

    working

---
+++ data/CV_Data_Analyst_Jobs.sql
@@ -0,0 +1,14 @@
+Select count (*) from data_analyst_jobs;
+
+--1793--
+
+Select *
+from data_analyst_jobs 
+LIMIT 10;
+
+
+--ExxonMobil--
+
+Select *
+From data_analyst_jobs
+WHERE location = Kentucky;
